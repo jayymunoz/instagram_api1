3e097ecdf03679bcfe6e9803132b8dcf5a3a1687
justin munoz <jayymunoz66@gmail.com>
2015-05-12 12:05:05 -0700

    video 11

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 
  	curl_setopt_array($ch, array(
  		CURLOPT_URL => $url,
- 		CURLOPT_RETURNTRANSFER => true,isfhieoh
+ 		CURLOPT_RETURNTRANSFER => true,isfhieohfhvifwhrgoie
  		CURLOPT_SSL_VERIFYPEER => false,
  		CURLOPT_SSL_VERIFYHOST => 2,
  		));
